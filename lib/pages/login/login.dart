0cce8912e0423ac87432c8159dccf1cda905ccf6
stastp <stasbombit4@gmail.com>
2026-03-02 20:02:24 +0300

    back

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,251 +1,130 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-import 'package:matrix/matrix.dart';
-
 import 'package:fluffychat/l10n/l10n.dart';
-import 'package:fluffychat/utils/localized_exception_extension.dart';
-import 'package:fluffychat/widgets/adaptive_dialogs/show_ok_cancel_alert_dialog.dart';
-import 'package:fluffychat/widgets/adaptive_dialogs/show_text_input_dialog.dart';
-import 'package:fluffychat/widgets/future_loading_dialog.dart';
-import 'package:fluffychat/widgets/matrix.dart';
-import '../../utils/platform_infos.dart';
-import 'login_view.dart';
+import 'package:fluffychat/widgets/layouts/login_scaffold.dart';
+import 'login.dart';
 
-class Login extends StatefulWidget {
-  final Client client;
-  const Login({required this.client, super.key});
+class LoginView extends StatelessWidget {
+  final LoginController controller;
+
+  const LoginView(this.controller, {super.key});
 
   @override
-  LoginController createState() => LoginController();
-}
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
 
-class LoginController extends State<Login> {
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  String? usernameError;
-  String? passwordError;
-  bool loading = false;
-  bool showPassword = false;
+    final homeserver = controller.widget.client.homeserver
+        ?.toString()
+        .replaceFirst('https://', '');
+    final title = homeserver == null
+        ? L10n.of(context).loginWithMatrixId
+        : L10n.of(context).logInTo(homeserver);
 
-  void toggleShowPassword() =>
-      setState(() => showPassword = !loading && !showPassword);
-
-  void login() async {
-    final matrix = Matrix.of(context);
-    if (usernameController.text.isEmpty) {
-      setState(() => usernameError = L10n.of(context).pleaseEnterYourUsername);
-    } else {
-      setState(() => usernameError = null);
-    }
-    if (passwordController.text.isEmpty) {
-      setState(() => passwordError = L10n.of(context).pleaseEnterYourPassword);
-    } else {
-      setState(() => passwordError = null);
-    }
-
-    if (usernameController.text.isEmpty || passwordController.text.isEmpty) {
-      return;
-    }
-
-    setState(() => loading = true);
-
-    _coolDown?.cancel();
-
-    try {
-      final username = usernameController.text;
-      AuthenticationIdentifier identifier;
-      if (username.isEmail) {
-        identifier = AuthenticationThirdPartyIdentifier(
-          medium: 'email',
-          address: username,
-        );
-      } else if (username.isPhoneNumber) {
-        identifier = AuthenticationThirdPartyIdentifier(
-          medium: 'msisdn',
-          address: username,
-        );
-      } else {
-        identifier = AuthenticationUserIdentifier(user: username);
-      }
-      final client = await matrix.getLoginClient();
-      await client.login(
-        LoginType.mLoginPassword,
-        identifier: identifier,
-        // To stay compatible with older server versions
-        // ignore: deprecated_member_use
-        user: identifier.type == AuthenticationIdentifierTypes.userId
-            ? username
-            : null,
-        password: passwordController.text,
-        initialDeviceDisplayName: PlatformInfos.clientName,
-      );
-    } on MatrixException catch (exception) {
-      setState(() => passwordError = exception.errorMessage);
-      return setState(() => loading = false);
-    } catch (exception) {
-      setState(() => passwordError = exception.toString());
-      return setState(() => loading = false);
-    }
-
-    if (mounted) setState(() => loading = false);
-  }
-
-  Timer? _coolDown;
-
-  void checkWellKnownWithCoolDown(String userId) {
-    _coolDown?.cancel();
-    _coolDown = Timer(
-      const Duration(seconds: 1),
-      () => _checkWellKnown(userId),
-    );
-  }
-
-  void _checkWellKnown(String userId) async {
-    if (mounted) setState(() => usernameError = null);
-    if (!userId.isValidMatrixId) return;
-    final oldHomeserver = widget.client.homeserver;
-    try {
-      var newDomain = Uri.https(userId.domain!, '');
-      widget.client.homeserver = newDomain;
-      DiscoveryInformation? wellKnownInformation;
-      try {
-        wellKnownInformation = await widget.client.getWellknown();
-        if (wellKnownInformation.mHomeserver.baseUrl.toString().isNotEmpty) {
-          newDomain = wellKnownInformation.mHomeserver.baseUrl;
-        }
-      } catch (_) {
-        // do nothing, newDomain is already set to a reasonable fallback
-      }
-      if (newDomain != oldHomeserver) {
-        await widget.client.checkHomeserver(newDomain);
-
-        if (widget.client.homeserver == null) {
-          widget.client.homeserver = oldHomeserver;
-          // okay, the server we checked does not appear to be a matrix server
-          Logs().v(
-            '$newDomain is not running a homeserver, asking to use $oldHomeserver',
+    return LoginScaffold(
+      appBar: AppBar(
+        leading: controller.loading ? null : const Center(child: BackButton()),
+        automaticallyImplyLeading: !controller.loading,
+        titleSpacing: !controller.loading ? 0 : null,
+        title: Text(title),
+      ),
+      body: Builder(
+        builder: (context) {
+          return AutofillGroup(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              children: <Widget>[
+                Hero(
+                  tag: 'info-logo',
+                  child: Image.asset('assets/banner_transparent.png'),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: TextField(
+                    readOnly: controller.loading,
+                    autocorrect: false,
+                    autofocus: true,
+                    onChanged: controller.checkWellKnownWithCoolDown,
+                    controller: controller.usernameController,
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.emailAddress,
+                    autofillHints: controller.loading
+                        ? null
+                        : [AutofillHints.username],
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.account_box_outlined),
+                      errorText: controller.usernameError,
+                      errorStyle: const TextStyle(color: Colors.orange),
+                      hintText: '@username:domain',
+                      labelText: L10n.of(context).matrixId,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: TextField(
+                    readOnly: controller.loading,
+                    autocorrect: false,
+                    autofillHints: controller.loading
+                        ? null
+                        : [AutofillHints.password],
+                    controller: controller.passwordController,
+                    textInputAction: TextInputAction.go,
+                    obscureText: !controller.showPassword,
+                    onSubmitted: (_) => controller.login(),
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock_outlined),
+                      errorText: controller.passwordError,
+                      errorStyle: const TextStyle(color: Colors.orange),
+                      suffixIcon: IconButton(
+                        onPressed: controller.toggleShowPassword,
+                        icon: Icon(
+                          controller.showPassword
+                              ? Icons.visibility_off_outlined
+                              : Icons.visibility_outlined,
+                          color: Colors.black,
+                        ),
+                      ),
+                      hintText: '******',
+                      labelText: L10n.of(context).password,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: theme.colorScheme.primary,
+                      foregroundColor: theme.colorScheme.onPrimary,
+                    ),
+                    onPressed: controller.loading ? null : controller.login,
+                    child: controller.loading
+                        ? const LinearProgressIndicator()
+                        : Text(L10n.of(context).login),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                if (homeserver != null)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: TextButton(
+                      onPressed: controller.loading
+                          ? () {}
+                          : controller.passwordForgotten,
+                      style: TextButton.styleFrom(
+                        foregroundColor: theme.colorScheme.error,
+                      ),
+                      child: Text(L10n.of(context).passwordForgotten),
+                    ),
+                  ),
+                const SizedBox(height: 16),
+              ],
+            ),
           );
-          final dialogResult = await showOkCancelAlertDialog(
-            context: context,
-            useRootNavigator: false,
-            title: L10n.of(
-              context,
-            ).noMatrixServer(newDomain.toString(), oldHomeserver.toString()),
-            okLabel: L10n.of(context).ok,
-            cancelLabel: L10n.of(context).cancel,
-          );
-          if (dialogResult == OkCancelResult.ok) {
-            if (mounted) setState(() => usernameError = null);
-          } else {
-            Navigator.of(context, rootNavigator: false).pop();
-            return;
-          }
-        }
-        usernameError = null;
-        if (mounted) setState(() {});
-      } else {
-        widget.client.homeserver = oldHomeserver;
-        if (mounted) {
-          setState(() {});
-        }
-      }
-    } catch (e) {
-      widget.client.homeserver = oldHomeserver;
-      usernameError = e.toLocalizedString(context);
-      if (mounted) setState(() {});
-    }
-  }
-
-  void passwordForgotten() async {
-    final input = await showTextInputDialog(
-      useRootNavigator: false,
-      context: context,
-      title: L10n.of(context).passwordForgotten,
-      message: L10n.of(context).enterAnEmailAddress,
-      okLabel: L10n.of(context).ok,
-      cancelLabel: L10n.of(context).cancel,
-      initialText: usernameController.text.isEmail
-          ? usernameController.text
-          : '',
-      hintText: L10n.of(context).enterAnEmailAddress,
-      keyboardType: TextInputType.emailAddress,
-    );
-    if (input == null) return;
-    final clientSecret = DateTime.now().millisecondsSinceEpoch.toString();
-    final response = await showFutureLoadingDialog(
-      context: context,
-      future: () => widget.client.requestTokenToResetPasswordEmail(
-        clientSecret,
-        input,
-        sendAttempt++,
+        },
       ),
     );
-    if (response.error != null) return;
-    final password = await showTextInputDialog(
-      useRootNavigator: false,
-      context: context,
-      title: L10n.of(context).passwordForgotten,
-      message: L10n.of(context).chooseAStrongPassword,
-      okLabel: L10n.of(context).ok,
-      cancelLabel: L10n.of(context).cancel,
-      hintText: '******',
-      obscureText: true,
-      minLines: 1,
-      maxLines: 1,
-    );
-    if (password == null) return;
-    final ok = await showOkAlertDialog(
-      useRootNavigator: false,
-      context: context,
-      title: L10n.of(context).weSentYouAnEmail,
-      message: L10n.of(context).pleaseClickOnLink,
-      okLabel: L10n.of(context).iHaveClickedOnLink,
-    );
-    if (ok != OkCancelResult.ok) return;
-    final data = <String, dynamic>{
-      'new_password': password,
-      'logout_devices': false,
-      'auth': AuthenticationThreePidCreds(
-        type: AuthenticationTypes.emailIdentity,
-        threepidCreds: ThreepidCreds(
-          sid: response.result!.sid,
-          clientSecret: clientSecret,
-        ),
-      ).toJson(),
-    };
-    final success = await showFutureLoadingDialog(
-      context: context,
-      future: () => widget.client.request(
-        RequestType.POST,
-        '/client/v3/account/password',
-        data: data,
-      ),
-    );
-    if (success.error == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(L10n.of(context).passwordHasBeenChanged)),
-      );
-      usernameController.text = input;
-      passwordController.text = password;
-      login();
-    }
   }
-
-  static int sendAttempt = 0;
-
-  @override
-  Widget build(BuildContext context) => LoginView(this);
-}
-
-extension on String {
-  static final RegExp _phoneRegex = RegExp(
-    r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$',
-  );
-  static final RegExp _emailRegex = RegExp(r'(.+)@(.+)\.(.+)');
-
-  bool get isEmail => _emailRegex.hasMatch(this);
-
-  bool get isPhoneNumber => _phoneRegex.hasMatch(this);
 }
--- lib/pages/sign_in/sign_in_page.dart
+++ lib/pages/sign_in/sign_in_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:url_launcher/url_launcher_string.dart';
+
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/config/themes.dart';
 import 'package:fluffychat/l10n/l10n.dart';
-import 'package:fluffychat/pages/sign_in/view_model/flows/check_homeserver.dart';
+import 'package:fluffychat/pages/sign_in/view_model/model/public_homeserver_data.dart';
 import 'package:fluffychat/pages/sign_in/view_model/sign_in_view_model.dart';
+import 'package:fluffychat/utils/localized_exception_extension.dart';
+import 'package:fluffychat/utils/sign_in_flows/check_homeserver.dart';
 import 'package:fluffychat/widgets/layouts/login_scaffold.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 import 'package:fluffychat/widgets/view_model_builder.dart';
@@ -20,33 +24,17 @@ class SignInPage extends StatelessWidget {
       create: () => SignInViewModel(Matrix.of(context), signUp: signUp),
       builder: (context, viewModel, _) {
         final state = viewModel.value;
-        
-        // Сразу запускаем подключение к matrix.cynk.ru при загрузке страницы
-        WidgetsBinding.instance.addPostFrameCallback((_) {
-          if (!state.isLoading) {
-            viewModel.setLoading(true);
-            // Используем существующий метод checkHomeserver
-            viewModel.checkHomeserver(
-              'matrix.cynk.ru',
-              onSuccess: () {
-                if (signUp) {
-                  Navigator.of(context).pushNamed('/register');
-                } else {
-                  Navigator.of(context).pushNamed('/login');
-                }
-              },
-              onError: (error) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(error.toString())),
-                );
-                viewModel.setLoading(false);
-              },
-            );
-          }
-        });
-
+        final publicHomeservers = state.filteredPublicHomeservers;
+        final selectedHomserver = state.selectedHomeserver;
         return LoginScaffold(
           appBar: AppBar(
+            leading:
+                state.loginLoading.connectionState == ConnectionState.waiting
+                ? CloseButton(
+                    onPressed: () =>
+                        viewModel.setLoginLoading(AsyncSnapshot.nothing()),
+                  )
+                : BackButton(onPressed: Navigator.of(context).pop),
             backgroundColor: theme.colorScheme.surface,
             surfaceTintColor: theme.colorScheme.surface,
             scrolledUnderElevation: 0,
@@ -57,18 +45,204 @@ class SignInPage extends StatelessWidget {
                   : L10n.of(context).login,
             ),
           ),
-          body: Center(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 16,
               children: [
-                Text(
-                  'Connecting to matrix.cynk.ru...',
-                  style: Theme.of(context).textTheme.titleMedium,
+                SelectableText(
+                  signUp
+                      ? L10n.of(context).signUpGreeting
+                      : L10n.of(context).signInGreeting,
+                  textAlign: .center,
                 ),
-                const SizedBox(height: 24),
-                const CircularProgressIndicator.adaptive(),
+                TextField(
+                  readOnly:
+                      state.publicHomeservers.connectionState ==
+                          ConnectionState.waiting ||
+                      state.loginLoading.connectionState ==
+                          ConnectionState.waiting,
+                  controller: viewModel.filterTextController,
+                  autocorrect: false,
+                  keyboardType: TextInputType.url,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: theme.colorScheme.secondaryContainer,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(99),
+                    ),
+                    errorText: state.publicHomeservers.error?.toLocalizedString(
+                      context,
+                    ),
+                    prefixIcon: const Icon(Icons.search_outlined),
+                    hintText: L10n.of(context).searchOrEnterHomeserverAddress,
+                  ),
+                ),
+                if (state.publicHomeservers.connectionState ==
+                    ConnectionState.done)
+                  Expanded(
+                    child: Material(
+                      borderRadius: BorderRadius.circular(
+                        AppConfig.borderRadius,
+                      ),
+                      clipBehavior: Clip.hardEdge,
+                      color: theme.colorScheme.surfaceContainerLow,
+                      child: RadioGroup<PublicHomeserverData>(
+                        groupValue: state.selectedHomeserver,
+                        onChanged: viewModel.selectHomeserver,
+                        child: ListView.builder(
+                          itemCount: publicHomeservers.length,
+                          itemBuilder: (context, i) {
+                            final server = publicHomeservers[i];
+                            final website = server.website;
+                            return RadioListTile.adaptive(
+                              value: server,
+                              enabled:
+                                  state.loginLoading.connectionState !=
+                                  ConnectionState.waiting,
+                              radioScaleFactor:
+                                  FluffyThemes.isColumnMode(context) ||
+                                      {
+                                        TargetPlatform.iOS,
+                                        TargetPlatform.macOS,
+                                      }.contains(theme.platform)
+                                  ? 2
+                                  : 1,
+                              title: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(server.name ?? 'Unknown'),
+                                  ),
+                                  if (website != null)
+                                    SizedBox.square(
+                                      dimension: 32,
+                                      child: IconButton(
+                                        icon: const Icon(
+                                          Icons.open_in_new_outlined,
+                                          size: 16,
+                                        ),
+                                        onPressed: () =>
+                                            launchUrlString(website),
+                                      ),
+                                    ),
+                                ],
+                              ),
+                              subtitle: Column(
+                                spacing: 4.0,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  if (server.features?.isNotEmpty == true)
+                                    Wrap(
+                                      spacing: 4.0,
+                                      runSpacing: 4.0,
+                                      children: [
+                                        ...?server.languages?.map(
+                                          (language) => Material(
+                                            borderRadius: BorderRadius.circular(
+                                              AppConfig.borderRadius,
+                                            ),
+                                            color: theme
+                                                .colorScheme
+                                                .tertiaryContainer,
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                    horizontal: 6.0,
+                                                    vertical: 3.0,
+                                                  ),
+                                              child: Text(
+                                                language,
+                                                style: TextStyle(
+                                                  fontSize: 10,
+                                                  color: theme
+                                                      .colorScheme
+                                                      .onTertiaryContainer,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        ...server.features!.map(
+                                          (feature) => Material(
+                                            borderRadius: BorderRadius.circular(
+                                              AppConfig.borderRadius,
+                                            ),
+                                            color: theme
+                                                .colorScheme
+                                                .secondaryContainer,
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                    horizontal: 6.0,
+                                                    vertical: 3.0,
+                                                  ),
+                                              child: Text(
+                                                feature,
+                                                style: TextStyle(
+                                                  fontSize: 10,
+                                                  color: theme
+                                                      .colorScheme
+                                                      .onSecondaryContainer,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  Text(
+                                    server.description ?? 'A matrix homeserver',
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  )
+                else
+                  Center(child: CircularProgressIndicator.adaptive()),
               ],
             ),
+          ),
+          bottomNavigationBar: AnimatedSize(
+            duration: FluffyThemes.animationDuration,
+            curve: FluffyThemes.animationCurve,
+            child:
+                selectedHomserver == null ||
+                    !publicHomeservers.contains(selectedHomserver)
+                ? const SizedBox.shrink()
+                : Material(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SafeArea(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: theme.colorScheme.primary,
+                            foregroundColor: theme.colorScheme.onPrimary,
+                          ),
+                          onPressed:
+                              state.loginLoading.connectionState ==
+                                  ConnectionState.waiting
+                              ? null
+                              : () => connectToHomeserverFlow(
+                                  selectedHomserver,
+                                  context,
+                                  viewModel.setLoginLoading,
+                                  signUp,
+                                ),
+                          child:
+                              state.loginLoading.connectionState ==
+                                  ConnectionState.waiting
+                              ? const CircularProgressIndicator.adaptive()
+                              : Text(L10n.of(context).continueText),
+                        ),
+                      ),
+                    ),
+                  ),
           ),
         );
       },
