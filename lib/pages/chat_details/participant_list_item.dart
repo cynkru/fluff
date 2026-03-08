8a9f9063d4bdcdd1cf91a9a6a680afd2c302f47a
stastp <stasbombit4@gmail.com>
2026-03-08 11:21:24 +0300

    z

--- lib/pages/chat_details/participant_list_item.dart
+++ lib/pages/chat_details/participant_list_item.dart
@@ -19,7 +19,7 @@ class ParticipantListItem extends StatelessWidget {
     final client = Matrix.of(context).client;
     try {
       final response = await client.httpClient.get(
-        'client.httpClient.get(Uri.parse('client.httpClient.get(Uri.parse('/_matrix/client/v3/profile/${user.id}',
+        Uri.parse('/_matrix/client/v3/profile/${user.id}'),
       );
       
       if (response.statusCode == 200) {
